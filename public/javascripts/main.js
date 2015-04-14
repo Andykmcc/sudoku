459d1ab94024d3778ce3c3018a3c78555a354b84
andykmccoy <andy.k.mccoy@gmail.com>
2015-04-14 02:41:58 -0700

    added comments to main and utilites

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,15 +3,20 @@ var utilities = require('./utilities');
 var touchUtilities = new (require('./touchUtilities'))();
 
 module.exports = (function(){
+  // simple detection for touch support. not full proof but gets the ob done. 
   var touchSupport = 'ontouchstart' in document.documentElement;
+  // set grid size e.g. 9x9 or 3x3... etc. 
   var gridSize = 9;
+  // cache the puzzel DOM element. 
   var sudoku = document.querySelector('.js-sudoku');
 
+  // listen for changes that bubble up from inputs. 
   sudoku.addEventListener('change', function(event){
     event.preventDefault();
     utilities.checkSudoku(event.currentTarget, gridSize);
   });
 
+  // if touch is available enable the touch UI.
   if(touchSupport){
     touchUtilities.init();
   }
--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -1,12 +1,19 @@
+/*
+  Various utility methods that deal with the DOM. 
+  Basically a few handy methods that would come 
+  with jQuery or zepto. 
+ */
 'use strict';
 var validateSudoku = require('./validateSudoku');
 
 module.exports = {
-
+  // converts the sudoku table into a 2D array. 
   convertTableToArray: function(table){
     var sudokuArr = [];
     var rows = table.querySelectorAll('tr');
 
+    // this could be done a a reduce but a simple for
+    // loop is for cross browser compat. and perf. 
     for(var i = 0, l = rows.length; i < l; i++){
       var rowArr = [];
       var row = rows[i];
@@ -17,22 +24,26 @@ module.exports = {
       }
       sudokuArr.push(rowArr);
     }
-
+    // return the 2D array.
     return sudokuArr;
   },
 
+  // this method determines if the sudoku is valid
   checkSudoku: function(sudoku, gridSize){
     var sudokuArr = this.convertTableToArray(sudoku);
 
     if(!this.isSudokuComplete(sudoku, gridSize)){
+      // if it is incomplete return styling to default state
       this.removeClass(sudoku, 'has-error');
       this.removeClass(sudoku, 'is-finished');
     }
     else if(validateSudoku.validate(sudokuArr, gridSize)){
+      // if complete and valid style with success styles. 
       this.removeClass(sudoku, 'has-error');
       this.addClass(sudoku, 'is-finished');
     }
     else{
+      // if complete but invalid add error styling. 
       this.addClass(sudoku, 'has-error');
       this.removeClass(sudoku, 'is-finished');
     }
@@ -60,6 +71,8 @@ module.exports = {
     return el;
   },
 
+  // returns an array or all the values in the sudoku.
+  // used as a faster way to check if sudoku is complete.
   getSudokuValues: function(sudoku){
     var inputs = sudoku.querySelectorAll('.js-input-number');
     var values = [];
